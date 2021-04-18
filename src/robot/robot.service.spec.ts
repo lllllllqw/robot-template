82f2c087b4b9998e76092c9c42372178d01ac7df
lllllllqw <465633856@qq.com>
2021-04-18 16:14:03 +0800

    tests: complete tests

--- src/robot/robot.service.spec.ts
+++ src/robot/robot.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { RobotService } from './robot.service';
 
@@ -6,6 +7,7 @@ describe('RobotService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [RobotService],
     }).compile();
 
